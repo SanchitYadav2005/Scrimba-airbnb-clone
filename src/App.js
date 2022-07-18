bd1d1f750090928c85ff4ee64f6f05f2c7d1eb42
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-18 11:46:20 +0530

    completed the prject yet the images not showing

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from "./components/Card"
-import img from "./images/katie-zaferes.png"
+import data from "./data"
 
 export default function App() {
+  const cards = data.map(iteam => {
+    return (
+      <Card
+        img={iteam.coverImg}
+        rating={iteam.stats.rating}
+        reviewCount={iteam.stats.reviewCount}
+        location={iteam.location}
+        title={iteam.title}
+        price={iteam.price}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-        img = {img}
-        rating = "5"
-        reviewCount = {6}
-        country = "USA"
-        title = "Life Lessons with Katie Zaferes"
-        price = {136}
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,12 @@ import starimg from "../images/star.png"
 export default function Card(props){
     return(
         <div className="card">
-            <img src={props.img} className="main-img" alt="img"/>
+            <img src={props.coverImg} className="main-img" alt="img"/>
             <div className="card-staus">
                 <img src={starimg} alt="img" className="card-star"/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount})  *</span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price} </span> /person</p>
